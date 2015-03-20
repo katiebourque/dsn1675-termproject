5cb3850c68f81d3a7ab1b0d6141b85c18ebe9993
katiebourque <kbourquedesigns@gmail.com>
2015-03-19 22:52:05 -0400

    Tabs
    Replaced SVGs
    Made tabs and made more info buttons function
    Fixed banner

--- js/main.js
+++ js/main.js
@@ -1,15 +1,24 @@
 var $btn = $('.btn');
 var $panel = $('.panel');
-var $btn1 =('.btn1');
-var $expanded = $('.expanded');
-
+var $btn1 = $('.btn1');
+var $text1 = $('.text1');
+var $btn2 = $('.btn2');
+var $text2 = $('.text2');
+var $btn3 = $('.btn3');
+var $text3 = $('.text3')
 
 $btn.on('click', function () {
 	$panel.toggleClass ('panel-open')
 });
 
 $btn1.on('click', function () {
-	$expanded.toggleClass ('expanded-p')
+	$text1.toggleClass ('text1-open')
 });
 
+$btn2.on('click', function () {
+	$text2.toggleClass ('text2-open')
+});
 
+$btn3.on('click', function () {
+	$text3.toggleClass ('text3-open')
+});
